4f898481bba80a1be0a284497a7b922726cde2a6
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-02-08 15:39:07 -0500

    windows exe

--- matlab/glow.m
+++ matlab/glow.m
@@ -11,7 +11,7 @@ validateattributes(Echar, {'numeric'}, {'positive', 'scalar'})
 %% binary
 cwd = fileparts(mfilename('fullpath'));
 exe = [cwd,filesep,'..', filesep, 'build', filesep, 'glow.bin'];
-
+if ispc, exe = [exe,'.exe']; end
 assert(exist(exe,'file')==2, 'compile Glow via setup_glow.m')
 %% time
 tvec = datevec(time);
