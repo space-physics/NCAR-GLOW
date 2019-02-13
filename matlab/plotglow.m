3afa02c8446c5c995f452722361212465455a843
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-02-13 15:55:34 -0500

    add optical plots [skip ci]

--- matlab/plotglow.m
+++ matlab/plotglow.m
@@ -5,11 +5,10 @@ validateattributes(glat, {'numeric'}, {'finite'})
 validateattributes(glon, {'numeric'}, {'finite'})
 %% Density profiles
 hp = figure;
-try
-sgtitle(hp, {[datestr(times),' deg.  (',num2str(glat),', ', num2str(glon),')']})
-end
-ax = subplot(1,2,1, 'parent', hp);
-set(ax, 'nextplot','add')
+ax = axes('parent', hp, 'nextplot', 'add');
+%try
+%sgtitle(hp, {[datestr(times),' deg.  (',num2str(glat),', ', num2str(glon),')']})
+%end
 
 semilogx(ax, iono.O, iono.altkm, 'DisplayName', 'O')
 semilogx(ax, iono.N2, iono.altkm, 'DisplayName', 'N_2')
@@ -21,7 +20,7 @@ semilogx(ax, iono.N2D, iono.altkm, 'DisplayName', 'N_2(D)')
 title(ax, 'Number Densities')
 xlabel(ax, 'Density [cm^-3]')
 ylabel(ax, 'altitude [km]')
-xlim(ax, [1e4,1e12])
+xlim(ax, [1e4,1e16])
 
 set(ax,'xscale','log')
 grid(ax, 'on')
@@ -29,7 +28,8 @@ legend(ax, 'show','location','northeast')
 
 %% Temperature Profiles
 
-ax = subplot(1,2,2, 'parent', hp);
+h=figure;
+ax = axes('parent',h);
 set(ax, 'nextplot','add')
 
 plot(ax, iono.Tn, iono.altkm, 'DisplayName', 'T_n')
@@ -40,11 +40,36 @@ ylabel(ax, 'altitude [km]')
 
 grid(ax, 'on')
 legend(ax, 'show','location','northeast')
-%%
-figure
-semilogx(iono.energy_bin_centers, iono.Eflux)
-grid('on')
-title('Particle Flux')
-xlabel('energy bin centers [eV]')
-ylabel('hemispherical flux [cm^{-2} s^{-1} eV^{-1}')
+%% particle flux
+ttxt = 'Particle Flux';
+hf=figure('Name', ttxt);
+ax = axes('parent', hf);
+semilogx(ax, iono.energy_bin_centers, iono.Eflux)
+grid(ax, 'on')
+title(ax, ttxt)
+xlabel(ax, 'energy bin centers [eV]')
+ylabel(ax, 'hemispherical flux [cm^{-2} s^{-1} eV^{-1}')
+%% optical emissions
+ttxt = 'Optical Emissions';
+ho = figure('Name', ttxt);
+ax = axes('parent', ho, 'nextplot', 'add');
+semilogx(ax, iono.A3371, iono.altkm, 'displayname', '3371')
+semilogx(ax, iono.A4278, iono.altkm, 'displayname', '4278')
+semilogx(ax, iono.A5200, iono.altkm, 'displayname', '5200')
+semilogx(ax, iono.A5577, iono.altkm, 'displayname', '5577')
+semilogx(ax, iono.A6300, iono.altkm, 'displayname', '6300')
+semilogx(ax, iono.A7320, iono.altkm, 'displayname', '7320')
+semilogx(ax, iono.A10400, iono.altkm, 'displayname', '10400')
+semilogx(ax, iono.A3644, iono.altkm, 'displayname', '3644')
+semilogx(ax, iono.A7774, iono.altkm, 'displayname', '7774')
+semilogx(ax, iono.A8446, iono.altkm, 'displayname', '8446')
+semilogx(ax, iono.A3726, iono.altkm, 'displayname', '3726')
+semilogx(ax, iono.LBH, iono.altkm, 'displayname', 'LBH')
+semilogx(ax, iono.A1356, iono.altkm, 'displayname', '1356')
+semilogx(ax, iono.A1493, iono.altkm, 'displayname', '1493')
+semilogx(ax, iono.A1304, iono.altkm, 'displayname', '1304')
+set(ax, 'xscale', 'log')
+grid(ax,'on')
+title(ax, ttxt)
+legend(ax, 'show')
 end
