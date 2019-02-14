c02cd2e0a3c0bc0698592a8271de5647b1cb0b16
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-02-14 13:33:10 -0500

    make matlab build robust

--- matlab/setup_glow_cmake.m
+++ matlab/setup_glow_cmake.m
@@ -1,16 +1,24 @@
 %% setup GLOW using CMake+Fortran compiler
 
+system('cmake --version')
+
+cwd = fileparts(mfilename('fullpath'));
+
+srcdir =   [cwd, filesep,'..'];
+builddir = [cwd, filesep,'..',filesep,'build'];
+tail = [' -S ', srcdir, ' -B ', builddir];
+
 if ispc
-  ccmd = 'cmake -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND" -S .. -B build';
+  ccmd = ['cmake -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND" ', tail];
 else
-  ccmd = 'cmake -S .. -B build';
+  ccmd = ['cmake ',tail];
 end
 
 [status, ret] = system(ccmd);
 if status~=0, error(ret), end
 disp(ret)
 
-[status, ret] = system('cmake --build build -j');
+[status, ret] = system(['cmake --build ',builddir,' -j']);
 if status~=0, error(ret), end
 disp(ret)
 
