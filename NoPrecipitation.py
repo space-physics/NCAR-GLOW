70ff35c42f73e43eedef481144a1954c4f0407ba
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-09-25 11:52:39 -0400

    init script for noprecip

---
+++ NoPrecipitation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+"""
+no particle precipitation
+"""
+import ncarglow as glow
+import ncarglow.plots as plot
+from datetime import datetime
+from matplotlib.pyplot import show
+
+
+time = datetime(2015, 12, 13, 10, 0, 0)
+glat = 65.1
+glon = -147.5
+# %% Number of energy bins
+Nbins = 250
+
+iono = glow.no_precipitation(time, glat, glon, Nbins)
+# %% simple plots
+plot.precip(iono['precip'])
+
+plot.ver(iono)
+
+show()
