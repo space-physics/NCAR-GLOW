08e1b2ca597a8ab9ead4a14d8fae252ce54b3aaa
scivision <scivision@users.noreply.github.com>
2023-09-01 13:54:11 -0400

    fs: use generic filesep

--- src/ncarglow/fortran/fsutils.f90
+++ src/ncarglow/fortran/fsutils.f90
@@ -17,9 +17,9 @@ if (len_trim(indir) < 1) error stop 'must provide path to expand'
 if (indir(1:1) /= '~') then
   expanduser = trim(adjustl(indir))
 elseif (len_trim(indir) < 3) then
-  expanduser = homedir()
+  expanduser = homedir() // "/"
 else
-  expanduser = homedir() // trim(adjustl(indir(3:)))
+  expanduser = homedir() // "/" // trim(adjustl(indir(3:)))
 endif
 
 end function expanduser
@@ -28,19 +28,16 @@ end function expanduser
 function homedir()
 
 character(:), allocatable :: homedir
-character :: filesep
 character(256) :: buf
 
 ! assume MacOS/Linux/BSD/Cygwin/WSL
-filesep = '/'
 call get_environment_variable("HOME", buf)
 
 if (len_trim(buf) == 0) then  ! Windows
   call get_environment_variable("USERPROFILE", buf)
-  filesep = char(92)
 endif
 
-homedir = trim(buf) // filesep
+homedir = trim(buf)
 
 end function homedir
 
